05d07db51b860d742138cf7d72c5e2b0fc21b780
lalalaohnyda-netizen <lalalaohnyda@gmail.com>
2026-05-14 10:56:25 +0200

    Update InputMixin.java

--- src/main/java/com/example/mixin/InputMixin.java
+++ src/main/java/com/example/mixin/InputMixin.java
@@ -9,10 +9,13 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ClientPlayerEntity.class)
 public class InputMixin {
+    // Этот редирект говорит игре, что экрана НЕТ, если это не чат.
+    // Это открывает возможность ходить в любом GUI (инвентарь, сундуки).
     @Redirect(method = "tickMovement", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;currentScreen:Lnet/minecraft/client/gui/screen/Screen;"))
-    private net.minecraft.client.gui.screen.Screen stopInventoryStop(MinecraftClient client) {
-        // Позволяет ходить в инвентаре, но стопит в чате
-        if (client.currentScreen instanceof ChatScreen) return client.currentScreen;
+    private net.minecraft.client.gui.screen.Screen bypassInventory(MinecraftClient client) {
+        if (client.currentScreen instanceof ChatScreen) {
+            return client.currentScreen;
+        }
         return null; 
     }
 }
